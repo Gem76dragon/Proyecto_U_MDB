8e7771d0bb675eb69cd7c2079d34ca6abefe5a27
Gem76dragon <gem76dragon@gmail.com>
2024-08-19 23:49:30 -0500

    Update __init__.py
    cambios

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
-from .claude_adapter import ClaudeAdapter, set_api_key
-from .file_adapter import FileAdapter
-from .streamlit_adapter import StreamlitAdapter
+from app.adapters.claude_adapter import ClaudeAdapter, set_api_key
+from app.adapters.file_adapter import FileAdapter
+from app.adapters.streamlit_adapter import StreamlitAdapter
 
 __all__ = ['ClaudeAdapter', 'set_api_key', 'FileAdapter', 'StreamlitAdapter']
