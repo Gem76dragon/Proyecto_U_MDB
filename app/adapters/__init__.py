99c3fc477072f4f7e09de5546d678a61a3f85902
Gem76dragon <gem76dragon@gmail.com>
2024-08-19 23:58:49 -0500

    cambios
    cambios

--- app/adapters/__init__.py
+++
@@ -1,5 +0,0 @@
-from .claude_adapter import ClaudeAdapter, set_api_key
-from .file_adapter import FileAdapter
-from .streamlit_adapter import StreamlitAdapter
-
-__all__ = ['ClaudeAdapter', 'set_api_key', 'FileAdapter', 'StreamlitAdapter']
--- app/ports/__init__.py
+++
@@ -1,5 +0,0 @@
-from adapters.claude_adapter import ClaudeAdapter, set_api_key
-from adapters.file_adapter import FileAdapter
-from adapters.streamlit_adapter import StreamlitAdapter
-
-__all__ = ['ClaudeAdapter', 'set_api_key', 'FileAdapter', 'StreamlitAdapter']
